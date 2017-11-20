76dc64bcc7f592bf519069ba9cd8da4774ca52dc
Euan Kemp <euank@euank.com>
2017-11-19 22:40:37 -0800

    frontend: implement basic oauth flow
    This does the oauth dance, gets the code, and then logs it to console.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-import GithubLogin from './github-login-component';
+import GithubLogin, {GithubOauthWindow} from './github-login-component';
 
 import logo from './logo.svg';
 import './App.css';
@@ -8,21 +13,32 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="Accounts">
+      <div className="WobscaleAccounts">
         <header className="Wobscale-Login">
           <img src={logo} className="Wobscale-logo" alt="logo" />
-          <h1 className="App-title">Login</h1>
         </header>
-        <p className="App-intro">
-          Login to your Wobscale Account
-        </p>
-        <GithubLogin />
-
-        <h2>Create an account</h2>
-        <p> If you don't have an account yet, login to create one </p>
+        <Router>
+          <div>
+            <Route exact path="/" component={MainPage}/>
+            <Route path="/github/oauth" component={GithubOauthWindow}/>
+          </div>
+        </Router>
       </div>
     );
   }
 }
+
+const MainPage = () => (
+  <div>
+    <h1 className="App-title">Login</h1>
+    <p>
+      Login to your Wobscale Account
+    </p>
+    <GithubLogin />
+
+    <h2>Create an account</h2>
+    <p> If you don't have an account yet, login to create one </p>
+  </div>
+);
 
 export default App;
--- frontend/src/github-login-component.js
+++ frontend/src/github-login-component.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import config from './config';
+import qs from 'query-string';
 
 class GithubLogin extends Component {
     constructor(props) {
@@ -18,27 +25,97 @@ class GithubLogin extends Component {
         // Used and modified under the terms of the MIT license.
 
         // TODO:
-        // 1. Open window at a new route for github login flow
         // 2. retrieve oauth response from the window
         // 3. Get the user
         // 4. Route to dashboard or create user depending on 3.'s result
+        window.github_response = undefined;
+        let windowDimensions = [
+            'width=1000',
+            'height=450',
+            'toolbar=0',
+            'scrollbars=1',
+            'status=1',
+            'resizable=1',
+            'location=1',
+            'menuBar=0'
+        ].join(',');
+        let win = window.open("/github/oauth", "Github Authorization", windowDimensions);
+
+        let waitClosed = window.setInterval(() => {
+          if(!win.closed) {
+            return;
+          }
+          window.clearInterval(waitClosed);
+          if (window.github_response) {
+            console.log(window.github_response);
+          }
+          this.setState(prev => ({
+              loggingIn: false,
+          }));
+        }, 200);
     }
 
     render() {
         if(this.state.loggingIn) {
             return (
-                <div class="logging-in">
+                <div className="logging-in">
                     <p>Please complete the login in the new window...</p>
                 </div>
             );
         } else {
             return (
-                <button onClick={this.loginClick}>
-                    Login with Github
-                </button>
+                <div>
+                    <button onClick={this.loginClick}>
+                        Login with Github
+                    </button>
+                </div>
             );
         }
     }
 };
+
+class GithubOauthWindow extends Component {
+    state = { error: null };
+
+    componentDidMount() {
+      let q = qs.parse(this.props.location.search);
+      if(q.code) {
+        window.opener.github_response = {
+          code: q.code,
+          state: q.state,
+        };
+        window.close();
+      }
+      fetch(config.api + "/github/authorize_url", {credentials: 'include'})
+        .then((res) => {
+          if(res.status > 300) {
+            throw new Error(res.statusText);
+          }
+          return res.text();
+        })
+        .then((url) => {
+          window.location = url;
+        })
+        .catch((err) => {
+            this.setState({error: err.toString()});
+        });
+    }
+
+    render() {
+      if(this.state.error) {
+        return (
+          <div>
+            <p>An error occurred: {this.state.error}</p>
+          </div>
+        );
+      } else {
+        return (
+          <p>Redirecting to Github Oauth Login</p>
+        );
+      }
+    }
+};
+
+export { GithubOauthWindow };
 
 export default GithubLogin;
