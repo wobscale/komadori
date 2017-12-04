106f8c42049a3b6d0c344de94199f1c592e397e6
Euan Kemp <euan.kemp@coreos.com>
2017-12-04 15:39:09 -0800

    Fiddle with cors more
    It's still gated by debug.

--- src/main.rs
+++ src/main.rs
@@ -142,6 +142,7 @@ impl Fairing for CORS {
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
 
         if request.method() == Method::Options {
+            response.set_status(Status::Ok);
             response.set_header(ContentType::Plain);
             response.set_sized_body(Cursor::new(""));
         }
