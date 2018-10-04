d5133307adefa8441b30381838b212491c20b766
Euan Kemp <euank@euank.com>
2018-10-04 15:35:28 -0700

    frontend: derive prod api from current host

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,8 +6,16 @@ function providers() {
   return p;
 }
 
+function api() {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:8000';
+  }
+  // canonically the backend is hosted at 'komadori.'
+  return `${window.location.protocol}//komadori.${window.location.host}`;
+}
+
 export default {
-  api: 'http://localhost:8000',
+  api: api(),
   // Well known admin group uuid
   adminGroup: 'b249560fc7c2463a872a79c9841d0139',
   loginProviders: providers(),
